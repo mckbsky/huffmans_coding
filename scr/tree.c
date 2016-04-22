9c952d287cb050196f0bc1acf9b291140cd8f390
mckbsky <maciej.brzeczkowski@protonmail.com>
2016-04-22 19:06:07 +0200

    Implemented tree generation and encoding

---
+++ scr/tree.c
@@ -0,0 +1,80 @@
+#include "tree.h"
+#include <stdlib.h>
+
+list *head;
+
+void generateBSTree(bsTree *histogram, int n, bsTree *root) {
+  while(n >= 0) {
+
+      bsTree *node = (bsTree *)malloc(sizeof(bsTree));
+      node->c = 0;
+      node->freq = histogram[n].freq + histogram[n - 1].freq;
+
+    if(root) {
+      if(n == 0) {
+        node->left = &histogram[n];
+        node->right = root;
+        root = node;
+      } else {
+        node->left = &histogram[n - 1];
+        node->right = &histogram[n];
+
+        bsTree *new_root = (bsTree *)malloc(sizeof(bsTree));
+        new_root->left = node;
+        new_root->right = root;
+        root = new_root;
+      }
+    } else {
+      node = root;
+    }
+    n -= 2;
+  }
+}
+
+void encode(bsTree *root) {
+  if(root->c != 0) {
+    saveCode(head, root->c);
+    delete_list_node(head);
+    return;
+  }
+  insert_list_node(head);
+  head->code = 1;
+  encode(root->right);
+  if(root->left != NULL) {
+    insert_list_node(head);
+    head->code = 0;
+    encode(root->left);
+  }
+  delete_list_node(head);
+}
+
+list* create_list(void) {
+    list *head = (list *)malloc(sizeof(list));
+    if (head != NULL) {
+        head->code = 2;
+        head->next = NULL;
+    }
+    return head;
+}
+
+
+
+void saveCode(list *head, char c) {
+
+}
+
+void insert_list_node(list *head) {
+  if(head) {
+    list *list_node = (list*)malloc(sizeof(list));
+    list_node->next = head;
+    head = list_node;
+  }
+  fprintf(stderr, "Empty list, can't add new element\n");
+}
+
+void delete_list_node(list *head) {
+  list *tmp = head;
+  head = head->next;
+  head->next = head->next->next;
+  free(tmp);
+}
--- scr/tree.h
+++ scr/tree.h
@@ -1,36 +1,66 @@
 #ifndef TREE_H
 #define TREE_H
 #include <stdio.h>
-#include "histogram.h"
 
 //drzewo poszukiwan binarnych: Binary Search Tree
-struct bsTreeTemplate {
-  int id;
-  struct bsTree *left, *right;
+struct bsTreeNode {
+  int freq;
+  char c;
+  struct bsTreeNode *left, *right;
 };
 
-typedef struct bsTreeTemplate bsTree;
-bsTree *tree = NULL;
+struct list_node {
+  unsigned int code:2;
+  struct list_node *next;
+};
 
-/*
- * TODO: Maciek - generowanie drzewa poszukiwań binarnych na podstawie
- * posortowanego histogramu
- */
+typedef struct list_node list;
+typedef struct bsTreeNode bsTree;
 
-void generateBSTree(histogram *hArray, bsTree *tree);
+bsTree *root = NULL;
+bsTree histogram[25];
 
-/*
- * TODO: Maciek - funckja zapisuje zakodowany ciąg zer i jedynek do pliku
- */
+list* create_list(void);
+void insert_list_node(list *head);
+void saveCode(list *head, char c);
+void delete_list_node(list *head);
 
-void encode(bsTree *tree, char *outputFile);
+
+
+void generateBSTree(bsTree *histogram, int n, bsTree *root);
+
+void encode(bsTree *root);
+
+void writeToFile(char *inputFile, char *outputFile);
 
 /*
  * TODO: Maciek, Mariusz: funkcja dekodująca z pliku zakodowany ciąg zer i
  * jedynek
  */
 
-void decode(bsTree *tree, char* inputFile);
+void decode(bsTree *root, char *inputFile);
 
+
+/*
+ * TODO: Mariusz - wypełnienie tablicy struktur kolejnymi literami i ustawienie
+ * int occ na 0
+ * NOTE: uwzględniać wielkośc liter?
+ */
+
+void prepareHistogram(bsTree *histogram);
+
+/*
+ * TODO: Mariusz - funkcja otwiera plik tekstowy o nazwie podanej jako argument
+ * wywołania i tworzy na jego podstawie histogram występowania poszczególnych
+ * liter
+ */
+void createHistogram(char* filename, bsTree *histogram);
+
+/*
+ * TODO: sortowanie względem ilości wystąpień danej litery
+ * NOTE: początkowo może to być jakieś proste sortowanie, docelowo qsort
+ */
+
+void quickSort(bsTree *histogram);
 
 #endif //tree.h
