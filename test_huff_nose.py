e2f5e1f378c6e42064fd9e873691c8f4bf2a5b48
mckbsky <maciej.brzeczkowski@protonmail.com>
2017-07-23 12:26:50 +0200

    Add tests using pythonNose

---
+++ test_huff_nose.py
@@ -0,0 +1,67 @@
+import sys
+import subprocess
+import os
+
+testString = 'Huffmans Coding()123;'
+inputFile = 'inputNoseTest'
+outputFile = 'outputNoseTest'
+decodedFile = 'decodedNoseTest'
+encoded = b'\xff\xf8_\xc9\xbf\xdf\xf7?\x7f\x93\xfc\xf9\xf7\xde\xf9\xff\x00'
+
+
+def testMake():
+    child = subprocess.Popen('make')
+    streamData = child.communicate()[0]
+    assert child.returncode == 0
+
+
+def testHuffmanString():
+    bashCommand = './huffman -s \"' + testString + '\" ' + outputFile
+    subprocess.call(bashCommand, shell=True)
+    file = open(outputFile, 'rb')
+    line = file.readline()
+    file.close()
+    os.remove(outputFile)
+    os.remove(outputFile + '_key')
+    assert line.lower().strip() == encoded.lower().strip()
+
+
+def testHuffmanFileEncoding():
+    file = open(inputFile, 'w')
+    file.write(testString)
+    file.close()
+
+    bashCommand = './huffman -e \"' + inputFile + '\" \"' + outputFile + '\"'
+    subprocess.call(bashCommand, shell=True)
+
+    file = open(outputFile, 'rb')
+    line = file.readline()
+    file.close()
+
+    os.remove(inputFile)
+    os.remove(outputFile)
+    os.remove(outputFile + '_key')
+
+    assert line.lower().strip() == encoded.lower().strip()
+
+
+def testHuffmanDecoding():
+    bashCommand = './huffman -s \"' + testString + '\" ' + outputFile
+    subprocess.call(bashCommand, shell=True)
+    bashCommand = './huffman -d ' + outputFile + ' ' + decodedFile + \
+        ' ' + outputFile + '_key'
+    subprocess.call(bashCommand, shell=True)
+
+    file = open(decodedFile, 'r')
+    line = file.readline()
+
+    os.remove(outputFile)
+    os.remove(decodedFile)
+
+    assert line == testString
+
+
+def testAuthors():
+    bashCommand = './huffman --authors'
+    returnCode = subprocess.call(bashCommand, shell=True)
+    assert returnCode == 0
